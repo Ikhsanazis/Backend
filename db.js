9f4c299cc902db5d3bd428597d90a5c55a3becfc
unknown <mxnzis@gmail.com>
2022-09-12 22:06:05 -0700

    fixing error

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const { Client, Pool } = require("pg");
 
 let connection;
@@ -14,14 +15,21 @@ if (process.env.ENV_MODE === "prod") {
   connection = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     port: process.env.DB_PORT || 5432,
   });
 }
 
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+
+const connect = async () => {
+  try {
+    const response = await connection.connect();
+    if (response) console.log("Connect to database");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+connect();
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,47 +1,53 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8001;
 
-const bodyParser = require('body-parser')
-const helmet = require('helmet')
-const dotenv = require('dotenv')
-dotenv.config()
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const usersRoutes = require('./Route/users')
-const recipesRoutes = require('./Route/recipes')
-const commentsRoutes = require('./Route/comments')
-const authRoutes = require('./Route/auth')
+const usersRoutes = require("./Route/users");
+const recipesRoutes = require("./Route/recipes");
+const commentsRoutes = require("./Route/comments");
+const authRoutes = require("./Route/auth");
 
-app.use(helmet())
-const cors = require('cors')
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(helmet());
+const cors = require("cors");
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // CORS
-const allowlist = ['https://localhost:3000/Loginexample',"http://localhost:3000/Registerlagi"]
+const allowlist = [
+  "https://localhost:3000",
+  "http://localhost:3000/Registerlagi",
+];
 const corsOptionsDelegate = function (req, callback) {
-  let corsOptions
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true }
+  let corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true };
   } else {
-    corsOptions = { origin: false }
+    corsOptions = { origin: false };
   }
-  callback(null, corsOptions)
-}
+  callback(null, corsOptions);
+};
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // ALL ROUTES
-app.use('/', cors(corsOptionsDelegate), usersRoutes)
-app.use('/', cors(corsOptionsDelegate), recipesRoutes)
-app.use('/', cors(corsOptionsDelegate), commentsRoutes)
-app.use('/', cors(corsOptionsDelegate), authRoutes)
+app.use("/", cors(corsOptionsDelegate), usersRoutes);
+app.use("/", cors(corsOptionsDelegate), recipesRoutes);
+app.use("/", cors(corsOptionsDelegate), commentsRoutes);
+app.use("/", cors(corsOptionsDelegate), authRoutes);
 
+app.use("*", (req, res) => {
+  res.send("Sukses");
+});
 // END OF BOTTOM CODE
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
