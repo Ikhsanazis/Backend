a28cc4ba25bf7ba321431affd25fee9151fdeee4
unknown <mxnzis@gmail.com>
2022-09-13 09:05:59 -0700

    update

--- Route/users.js
+++ Route/users.js
@@ -1,22 +1,20 @@
-const Router = require('express').Router()
-const controller = require('../Controller/usersController')
-// const middleware = require('../middleware/verifyToken')
-const upload = require('../Middleware/multer')
+const Router = require("express").Router();
+const controller = require("../Controller/usersController");
+const upload = require("../Middleware/multer");
 
 // GET USERS
-Router.get('/users',  controller.getUsers)
+Router.get("/users", controller.getUsers);
 
 // POST USERS
-Router.post('/users/add', controller.addUser)
+Router.post("/users/add", controller.addUser);
 
 // PATCH USERS
-Router.patch('/users/edit/:id',upload, controller.editUser)
-Router.patch('/users/editprofile/:id',upload, controller.editUserProfile)
-
+Router.patch("/users/edit/:id", upload, controller.editUser);
+Router.patch("/users/editprofile/:id", upload, controller.editUserProfile);
 
 // DELETE USERS ROUTES
-Router.delete('/users/delete', controller.deleteUser)
+Router.delete("/users/delete", controller.deleteUser);
 
-module.exports = Router
+module.exports = Router;
 
 // middleware.verifyToken
